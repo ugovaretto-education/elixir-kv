1d16e66938992b1637a9a23e6a13061168fbe35e
Ugo Varetto <ugovaretto@gmail.com>
2024-01-06 14:47:17 +0800

    Fixed runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,8 +1,22 @@
+import Config
 config :kv, :routing_table, [{?a..?z, node()}]
 
+# running the same application on two separate systems
 if config_env() == :prod do
   config :kv, :routing_table, [
-    {?a..?m, :"foo@system76.local"},
-    {?n..?z, :"bar@metabox.local"}
+    {?a..?m, :"kvapp@system76.local"},
+    {?n..?z, :"kvapp@metabox.local"}
   ]
 end
+
+# running two separate applications, requires setting
+# two releases, one per app.
+# When running on the same node the bar application should not contain the server
+# since only a single frontend listening on a port is needed, or configure the port
+# in runtime.exs
+# if config_env() == :prod do
+#   config :kv, :routing_table, [
+#     {?a..?m, :"kvapp@system76.local"},
+#     {?n..?z, :"kvapp@metabox.local"}
+#   ]
+# end
